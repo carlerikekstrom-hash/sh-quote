1569cdcaed737214266ace354b31bc14e2fe92f0
carlerikekstrom-hash <carlerikekstrom@gmail.com>
2026-05-13 14:57:46 -0600

    Add files via upload

---
+++ sw-quote_1.js
@@ -0,0 +1,21 @@
+const CACHE = 'sh-quote-v4';
+
+self.addEventListener('install', e => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  // Network-first: always try network, fall back to cache for offline use
+  e.respondWith(
+    fetch(e.request).then(res => {
+      const clone = res.clone();
+      caches.open(CACHE).then(c => c.put(e.request, clone));
+      return res;
+    }).catch(() => caches.match(e.request).then(r => r || caches.match('./index.html')))
+  );
+});
